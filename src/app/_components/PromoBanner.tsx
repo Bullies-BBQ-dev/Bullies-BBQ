3ba25dff86a708256613e0038175b511d33e3e16
Daniel Perez <danielperezf98@gmail.com>
2024-03-04 14:37:58 -0800

    removed commented code

--- src/app/_components/PromoBanner.tsx
+++ src/app/_components/PromoBanner.tsx
@@ -1,33 +1,21 @@
 "use client";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { IoMdClose } from "react-icons/io";
 
 export function PromoBanner() {
   const [isOpen, setIsOpen] = useState(true);
 
-  //   useEffect(() => {
-  //     // const isBannerClosed = sessionStorage.getItem("isBannerClosed");
-  //     // if (isBannerClosed) {
-  //     setIsOpen(false);
-  //     // }
-  //   }, []);
-
   const handleClose = () => {
     setIsOpen(false);
-    // sessionStorage.setItem("isBannerClosed", "true");
   };
 
   return (
     <>
       {isOpen && (
         <div className="w-full bg-yellow-400 flex justify-between  py-1 relative overflow-hidden">
-          <Link
-            href="/catering"
-            className="w-full hover:underline"
-            // onClick={handleClose}
-          >
+          <Link href="/catering" className="w-full hover:underline">
             <div className="animate-slide w-full flex ">
               <span>Please check out our catering menu!</span>
             </div>
