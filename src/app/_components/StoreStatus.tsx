b726466a70aa5301f5bab2fd4296e58fc78de268
Kevin Lay <kevinlay@live.com>
2024-03-11 16:23:38 -0700

    Fixed-store-status-bug

--- src/app/_components/StoreStatus.tsx
+++ src/app/_components/StoreStatus.tsx
@@ -6,16 +6,16 @@ export function StoreStatus() {
     const now = new Date();
 
     const openTime = new Date();
-    openTime.setHours(11, 0, 0); // Set opening time to 11am
+    openTime.setUTCHours(15, 0, 0); // Set opening time to 11am
 
     const closeTime = new Date();
-    closeTime.setHours(17, 0, 0); // Set closing time to 8pm
+    closeTime.setUTCHours(24, 0, 0); // Set closing time to 8pm
 
     const openingSoonTime = new Date(openTime);
-    openingSoonTime.setHours(openTime.getHours() - 1); // Set opening soon time to 1 hour before opening
+    openingSoonTime.setUTCHours(openTime.getUTCHours() - 1); // Set opening soon time to 1 hour before opening
 
     const closingSoonTime = new Date(closeTime);
-    closingSoonTime.setHours(closeTime.getHours() - 1); // Set closing soon time to 1 hour before closing
+    closingSoonTime.setUTCHours(closeTime.getUTCHours() - 1); // Set closing soon time to 1 hour before closing
 
     if (now >= openTime && now <= closeTime) {
       if (now >= closingSoonTime) {
@@ -23,6 +23,8 @@ export function StoreStatus() {
       } else {
         return "open";
       }
+    } else if (now > closeTime && now >= openingSoonTime) {
+      return "closed";
     } else if (now >= openingSoonTime) {
       return "openingSoon";
     } else {
